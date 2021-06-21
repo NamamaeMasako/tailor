dbe5dd169d16951c20a4445649228b5e1b2ca359
Alex <c23026730@gmail.com>
2021-06-21 18:13:34 +0800

    準備材料介面

--- frontend/src/components/_base/headerNav.vue
+++ frontend/src/components/_base/headerNav.vue
@@ -1,15 +1,15 @@
 <template>
 <nav class="container-fluid py-3 fixed-top">
     <div class="row">
-        <div class="col-5">
+        <div class="col-11">
             <div class="row">
-                <img src="../../assets/images/girl.svg" class="col-2" alt="avator">
-                <div class="col-9">
+                <img src="../../assets/images/girl.svg" class="col-1" alt="avator">
+                <div class="col-3">
                     <div class="row h-100">
                         <div class="col-12 align-self-start">
                             <div class="row">
-                                <div class="col-6 d-flex align-items-center">{{loginData.name}}<span class="badge badge-warning ml-1">VIP</span></div>
-                                <div class="col-6">Lv. {{loginData.level}}</div>
+                                <div class="col-8 d-flex align-items-center">{{loginData.name}}<span class="badge badge-warning ml-1">VIP</span></div>
+                                <div class="col-4 text-right">Lv. {{loginData.level}}</div>
                             </div>
                         </div>
                         <div class="col-12 align-self-end">
@@ -30,10 +30,81 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-1">
-                    <b-button variant="danger" v-on:click="logout">登出</b-button>
+                <div class="col-4">
+                    <div class="col-12 align-self-end">
+                        <div class="row">
+                            <div class="col-12">
+                                <span class="font-weight-light">原料倉庫</span>
+                                <div class="row">
+                                    <div class="col-3">容量</div>
+                                    <div class="col-9">
+                                        <div class="progress my-2" style="height: 5px">
+                                            <div class="progress-bar bg-warning" role="progressbar" style="width: 28%"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-12 mt-1">
+                                    <div class="row">
+                                        <div class="col-3 px-1">
+                                            <div class="d-flex align-items-center justify-content-center bg-dark text-light rounded-pill py-1">
+                                                <i class="fas fa-cannabis"></i><span class="badge badge-pill badge-light ml-1">22</span>
+                                            </div>
+                                        </div>
+                                        <div class="col-3 px-1">
+                                            <div class="d-flex align-items-center justify-content-center bg-dark text-light rounded-pill py-1">
+                                                <i class="fas fa-feather-alt"></i><span class="badge badge-pill badge-light ml-1">22</span>
+                                            </div>
+                                        </div>
+                                        <div class="col-3 px-1">
+                                            <div class="d-flex align-items-center justify-content-center bg-dark text-light rounded-pill py-1">
+                                                <i class="fas fa-bug"></i><span class="badge badge-pill badge-light ml-1">22</span>
+                                            </div>
+                                        </div>
+                                        <div class="col-3 px-1">
+                                            <div class="d-flex align-items-center justify-content-center bg-dark text-light rounded-pill py-1">
+                                                <i class="fas fa-gem"></i><span class="badge badge-pill badge-light ml-1">22</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="col-12 align-self-end">
+                        <div class="row">
+                            <div class="col-12">
+                                <span class="font-weight-light">exp.</span>
+                                <div class="progress" style="height: 5px">
+                                    <div class="progress-bar bg-warning" role="progressbar" style="width: 28%"></div>
+                                </div>
+                                <div class="col-12 mt-1">
+                                    <div class="row">
+                                        <div class="col-3">
+                                            <div class="d-flex align-items-center bg-dark text-light">
+                                                絲<span class="badge badge-pill badge-light ml-1">22</span>
+                                            </div>
+                                        </div>
+                                        <div class="col-3 d-flex align-items-center bg-dark text-light">
+                                            皮<span class="badge badge-pill badge-light ml-1">22</span>
+                                        </div>
+                                        <div class="col-3 d-flex align-items-center bg-dark text-light">
+                                            皮<span class="badge badge-pill badge-light ml-1">22</span>
+                                        </div>
+                                        <div class="col-3 d-flex align-items-center bg-dark text-light">
+                                            皮<span class="badge badge-pill badge-light ml-1">22</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+        </div>
+        <div class="col-1">
+            <b-button variant="danger" v-on:click="logout">登出</b-button>
         </div>
     </div>
 </nav>
